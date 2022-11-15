0c00f5f990c9762da63ec011dbb62075ef3e2edf
muharrem gem <muharremgem28@gmail.com>
2022-11-15 15:33:58 +0300

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,8 @@ import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
   const navigate = useNavigate();
+  const user = false;
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="secondary">
@@ -20,8 +22,8 @@ export default function Navbar() {
           >
             breaking news
           </Typography>
-
-          <Button color="inherit">Login</Button>
+          {user && <Button color="inherit">Logout</Button>}
+          {!user && <Button color="inherit">Login</Button>}
         </Toolbar>
       </AppBar>
     </Box>
