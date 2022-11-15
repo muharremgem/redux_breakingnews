68624517854ff410f2579e98d31007be3b7a4926
muharrem gem <muharremgem28@gmail.com>
2022-11-15 17:03:48 +0300

    provider

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,7 +19,7 @@ export default function Login() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(setUser(""));
+    dispatch(setUser({ email, password }));
     setEmail("");
     setPassword("");
     navigate("/");
